696ff4935e016d1f15ee28784bf7d16c262862a1
Jarek Glowacki <jarekwg@gmail.com>
2016-11-22 16:59:07 +1100

    Improve pipinstallability

--- myob/__init__.py
+++ myob/__init__.py
@@ -1,6 +1,5 @@
 from .api import Myob  # noqa
-from .credentials import PartnerCredentials  # noqa
 
-VERSION = (0, 1, 3)
+VERSION = (0, 1, 4)
 
 __version__ = '.'.join(str(x) for x in VERSION[:(2 if VERSION[2] == 0 else 3)])  # noqa
