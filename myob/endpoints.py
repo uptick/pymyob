1a3a74e31b117a2eeec19d238c04049e7ea6d2fc
Jarek Glowacki <jarekwg@outlook.com>
2016-12-05 13:40:20 +1100

    Get some consistency going.

--- myob/endpoints.py
+++ myob/endpoints.py
@@ -10,30 +10,30 @@ ENDPOINTS = {
     '': {
         'plural': 'companyfiles',
         'methods': [
-            (ALL, '', 'Returns a list of company files.'),
-            (GET, '[id]', 'Lists endpoints available for a company file.'),
-            (GET, 'Info', 'Returns API build information for each individual endpoint'),
+            (ALL, '', 'Return a list of company files.'),
+            (GET, '[id]', 'List endpoints available for a company file.'),
+            (GET, 'Info', 'Return API build information for each individual endpoint.'),
         ],
     },
     '[company_id]/Contact': {
         'plural': 'contacts',
         'methods': [
             (ALL, '', 'Return all contact types for an AccountRight company file'),
-            (ALL, 'Customer', 'Returns all customer contacts for an AccountRight company file.'),
-            (GET, 'Customer/[uid]', 'Returns selected customer contact.'),
-            (PUT, 'Customer/[uid]', 'Updates selected customer contact.'),
-            (POST, 'Customer', 'Creates new customer contact.'),
-            (DELETE, 'Customer/[uid]', 'Deletes selected customer contact.'),
+            (ALL, 'Customer', 'Return all customer contacts for an AccountRight company file.'),
+            (GET, 'Customer/[uid]', 'Return selected customer contact.'),
+            (PUT, 'Customer/[uid]', 'Update selected customer contact.'),
+            (POST, 'Customer', 'Create new customer contact.'),
+            (DELETE, 'Customer/[uid]', 'Delete selected customer contact.'),
         ],
     },
     '[company_id]/Sale/Invoice': {
         'plural': 'invoices',
         'methods': [
-            (ALL, '', 'Return all sale invoice types for an AccountRight company file'),
-            (GET, 'Item', 'Return item type sale invoices for an AccountRight company file'),
-            (POST, 'Item', 'Create item type sale invoices for an AccountRight company file'),
-            (PUT, 'Item', 'Update item type sale invoices for an AccountRight company file'),
-            (DELETE, 'Item', 'Delete item type sale invoices for an AccountRight company file')
+            (ALL, '', 'Return all sale invoice types for an AccountRight company file.'),
+            (GET, 'Item', 'Return item type sale invoices for an AccountRight company file.'),
+            (POST, 'Item', 'Create item type sale invoices for an AccountRight company file.'),
+            (PUT, 'Item', 'Update item type sale invoices for an AccountRight company file.'),
+            (DELETE, 'Item', 'Delete item type sale invoices for an AccountRight company file.')
         ]
     }
 }
