4bf1826710983bd625092b943430639b85bdd500
Jarek Glowacki <jarekwg@gmail.com>
2019-06-22 09:24:04 +1000

    Add a test

--- tests/managers.py
+++ tests/managers.py
@@ -27,6 +27,8 @@ class QueryParamTests(TestCase):
         self.assertParamsEqual({'DisplayID__gt': '5-0000'}, {'$filter': "DisplayID gt '5-0000'"})
         self.assertParamsEqual({'DateOccurred__lt': '2013-08-30T19:00:59.043'}, {'$filter': "DateOccurred lt '2013-08-30T19:00:59.043'"})
         self.assertParamsEqual({'Type': ('Customer', 'Supplier'), 'DisplayID__gt': '5-0000'}, {'$filter': "Type eq 'Customer' or Type eq 'Supplier' and DisplayID gt '5-0000'"})
+        self.assertParamsEqual({'IsActive': True}, {'$filter': "IsActive eq true"})
+        self.assertParamsEqual({'IsActive': False}, {'$filter': "IsActive eq false"})
 
     def test_datetime_filter(self):
         self.assertParamsEqual({'DateOccurred__lt': datetime(1992, 11, 14)}, {'$filter': "DateOccurred lt datetime'1992-11-14 00:00:00'"})
