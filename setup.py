896d7a4c8ab9119ce60db617693630ac4011d3f7
Jarek Głowacki <jarekwg@gmail.com>
2020-04-20 12:49:28 +1000

    Curious how badge will render when i omit minor version classifiers..

--- setup.py
+++ setup.py
@@ -24,13 +24,14 @@ try:
         url='https://github.com/uptick/pymyob',
         keywords=['myob'],
         classifiers=[
-            'Development Status :: 2 - Pre-Alpha',
+            'Development Status :: 5 - Production/Stable',
             'Environment :: Web Environment',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
             'Topic :: Office/Business',
         ],
         install_requires=[
