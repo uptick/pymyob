4626518810bef8608f9bae746d917764e4724e54
James Foxall <james@getbusi.com>
2021-07-06 15:09:27 +1000

    Add Inventory/ItemPriceMatrix endpoints and tests

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -344,27 +344,33 @@ class EndpointTests(TestCase):
     def test_inventory(self):
         self.assertEqual(repr(self.companyfile.inventory), (
             "InventoryManager:\n"
-            "                 adjustment() - Return all inventory adjustments for an AccountRight company file.\n"
-            "       delete_adjustment(uid) - Delete selected inventory adjustment.\n"
-            "             delete_item(uid) - Delete selected inventory item.\n"
-            "         delete_location(uid) - Delete selected inventory location.\n"
-            "          get_adjustment(uid) - Return selected inventory adjustment.\n"
-            "                get_item(uid) - Return selected inventory item.\n"
-            "            get_location(uid) - Return selected inventory location.\n"
-            "                       item() - Return all inventory items for an AccountRight company file.\n"
-            "                   location() - Return all inventory locations for an AccountRight company file.\n"
-            "        post_adjustment(data) - Create new inventory adjustment.\n"
-            "              post_item(data) - Create new inventory item.\n"
-            "          post_location(data) - Create new inventory location.\n"
-            "    put_adjustment(uid, data) - Update selected inventory adjustment.\n"
-            "          put_item(uid, data) - Update selected inventory item.\n"
-            "      put_location(uid, data) - Update selected inventory location."
+            "                      adjustment() - Return all inventory adjustments for an AccountRight company file.\n"
+            "            delete_adjustment(uid) - Delete selected inventory adjustment.\n"
+            "                  delete_item(uid) - Delete selected inventory item.\n"
+            "              delete_location(uid) - Delete selected inventory location.\n"
+            "               get_adjustment(uid) - Return selected inventory adjustment.\n"
+            "                     get_item(uid) - Return selected inventory item.\n"
+            "          get_itempricematrix(uid) - Return selected inventory item price matrix.\n"
+            "                 get_location(uid) - Return selected inventory location.\n"
+            "                            item() - Return all inventory items for an AccountRight company file.\n"
+            "                 itempricematrix() - Return all inventory item price matrices for an AccountRight company file.\n"
+            "                        location() - Return all inventory locations for an AccountRight company file.\n"
+            "             post_adjustment(data) - Create new inventory adjustment.\n"
+            "                   post_item(data) - Create new inventory item.\n"
+            "               post_location(data) - Create new inventory location.\n"
+            "         put_adjustment(uid, data) - Update selected inventory adjustment.\n"
+            "               put_item(uid, data) - Update selected inventory item.\n"
+            "    put_itempricematrix(uid, data) - Update selected inventory item price matrix.\n"
+            "           put_location(uid, data) - Update selected inventory location."
         ))
         self.assertEndpointReached(self.companyfile.inventory.item, {}, 'GET', f'/{CID}/Inventory/Item/')
         self.assertEndpointReached(self.companyfile.inventory.get_item, {'uid': UID}, 'GET', f'/{CID}/Inventory/Item/{UID}/')
         self.assertEndpointReached(self.companyfile.inventory.put_item, {'uid': UID, 'data': DATA}, 'PUT', f'/{CID}/Inventory/Item/{UID}/')
         self.assertEndpointReached(self.companyfile.inventory.post_item, {'data': DATA}, 'POST', f'/{CID}/Inventory/Item/')
         self.assertEndpointReached(self.companyfile.inventory.delete_item, {'uid': UID}, 'DELETE', f'/{CID}/Inventory/Item/{UID}/')
+        self.assertEndpointReached(self.companyfile.inventory.itempricematrix, {}, 'GET', f'/{CID}/Inventory/ItemPriceMatrix/')
+        self.assertEndpointReached(self.companyfile.inventory.get_itempricematrix, {'uid': UID}, 'GET', f'/{CID}/Inventory/ItemPriceMatrix/{UID}/')
+        self.assertEndpointReached(self.companyfile.inventory.put_itempricematrix, {'uid': UID, 'data': DATA}, 'PUT', f'/{CID}/Inventory/ItemPriceMatrix/{UID}/')
         self.assertEndpointReached(self.companyfile.inventory.location, {}, 'GET', f'/{CID}/Inventory/Location/')
         self.assertEndpointReached(self.companyfile.inventory.get_location, {'uid': UID}, 'GET', f'/{CID}/Inventory/Location/{UID}/')
         self.assertEndpointReached(self.companyfile.inventory.put_location, {'uid': UID, 'data': DATA}, 'PUT', f'/{CID}/Inventory/Location/{UID}/')
